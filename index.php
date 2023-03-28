7e7cc0153c38fa8412579d0edca92baa7048fb0a
Matthew Onilude <m.onilude@rgu.ac.uk>
2023-03-28 12:08:49 +0000

    adding php files

---
+++ index.php
@@ -0,0 +1,56 @@
+<link rel="stylesheet" href="assets/style.css"/>    
+
+
+<h1>Hello World</h1>
+
+<?php
+// Start the session and check if the user is logged in
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header('Location: login.php');
+  exit;
+}
+
+// Connect to MySQL database
+require_once 'db_connect.php';
+
+// Get the user's details from the database
+$user_id = $_SESSION['user_id'];
+$query = "SELECT * FROM users WHERE id = $user_id";
+$result = mysqli_query($conn, $query);
+$user = mysqli_fetch_assoc($result);
+
+// Close the database connection
+// mysqli_close($conn);
+?>
+
+<!-- HTML landing page -->
+<h1>Welcome, <?php echo $user['username']; ?>!</h1>
+<p>Your email address is: <?php echo $user['email']; ?></p>
+<p><a href="logout.php">Log out</a></p>
+
+<?php
+  // Connect to MySQL database
+  // require_once 'assets/db_connect.php';
+
+  // fetch all stories from database
+  $stmt = $conn->prepare("SELECT * FROM stories ORDER BY created_at DESC");
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $stories = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<h1>Latest Stories</h1>
+	<div>
+		<?php foreach ($stories as $story): ?>
+			<div>
+				<h2><?php echo $story['title']; ?></h2>
+				<p>By <?php echo $story['author']; ?></p>
+				<?php if ($story['image']): ?>
+					<img src="<?php echo $story['image']; ?>" alt="Story Image">
+				<?php endif; ?>
+				<p><?php echo substr($story['content'], 0, 200); ?>...</p>
+				<p><a href="story.php?id=<?php echo $story['id']; ?>">Read more</a></p>
+			</div>
+		<?php endforeach; ?>
+	</div>
---
+++ story.php
@@ -0,0 +1,34 @@
+<?php
+     
+
+    // Connect to MySQL database
+    require_once 'db_connect.php';
+
+    // get story id from URL parameter
+    $id = $_GET['id'];
+
+    // fetch story from database
+    $stmt = $conn->prepare("SELECT * FROM stories WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $story = $result->fetch_assoc();
+
+   
+    // check if story exists
+    if (!$story) {
+        echo "Story not found";
+        exit();
+    }
+    
+    
+?>
+<title><?php echo $story['title']; ?></title>
+<h1><?php echo $story['title']; ?></h1>
+<p>By <?php echo $story['author']; ?></p>
+<p>Category: <?php echo $story['category']; ?></p>
+<?php if ($story['image']): ?>
+    <img src="<?php echo $story['image']; ?>" alt="Story Image">
+<?php endif; ?>
+<p><?php echo $story['content_html']; ?></p>
+<p>Published on <?php echo $story['published_at']; ?></p>
